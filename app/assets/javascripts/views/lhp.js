4f39ef2ef3509103f8c5e132d4bac2ccaed03b35
Slawomir Demichowicz <slawomir.demichowicz@gmail.com>
2012-12-17 00:56:57 +0000

    Get Map button changes to Get Directions if more than one location is added.

--- app/assets/javascripts/views/lhp.js
+++ app/assets/javascripts/views/lhp.js
@@ -9,34 +9,43 @@ function(_, $, Backbone, TextInput){
 
   var LHP = Backbone.View.extend({
     events: {
-      'click div.add-destination': 'add_destination'
-    },
-    inputs: [],
-    initialize: function(){
-      var _this = this;
-
-
+      'click div.add-destination': function(){
+        var _this = this;
+        _this.add_destination();
+        _this.update_search_button_text();
+      }
     },
     render: function(){
       var _this = this,
           input = new TextInput();
 
-      _this.inputs.push(input); //TODO: Make it searchable & idexable, useless for now
+
       _this.$el.find('form').prepend(input.render().el);
 
-      _this.$el.fadeIn();
 
+
+      _this.$el.fadeIn();
       _this.$el.find('input').first().focus();
+
     },
     add_destination: function(){
       var _this = this,
           input = new TextInput();
 
-      _this.inputs.push(input);
-
       _this.$el.find('form > div.location-input').last().after(input.render().el);
 
+      input.on('remove', _this.update_search_button_text, _this);
+    },
+    update_search_button_text: function(){
+      var _this = this,
+          $el = _this.$el,
+          main_search_button = $el.find('button#main-search-button');
 
+      if($el.find('input').length > 1){
+        main_search_button.text('Get Directions');
+      } else {
+        main_search_button.text('Get Map');
+      }
     }
   });
 
--- app/assets/javascripts/views/text_input.js
+++ app/assets/javascripts/views/text_input.js
@@ -8,15 +8,36 @@ function(_, $, Backbone){
 
   var _template_text = ['<input name="location" type="text" class="search-from" placeholder="Type in Location"/>',
                    '<button class="close">&times;</button>'].join(''),
+      _view_counter = 0,
 
   TextInput = Backbone.View.extend({
+    events: {
+      'click button.close': function(){
+        var _this = this;
+        _view_counter--;
+        _this.remove();
+        _this.trigger('remove');
+      }
+    },
     template: _.template(_template_text),
     className: 'location-input',
+    initialize: function(){
+      _view_counter++;
+    },
+    show_close_icon: function(){
+      var _this = this;
+      if(_view_counter === 1){
+        _this.$el.find('button.close').hide();
+      } else {
+        _this.$el.find('button.close').show();
+      }
+    },
     render: function(){
       var _this = this,
           $el   = _this.$el;
 
       $el.html(_this.template({}));
+      _this.show_close_icon();
       return this;
     }
   });
